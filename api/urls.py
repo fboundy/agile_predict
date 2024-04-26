e77d918b6f5e9f2db0a158ab74335026b3e56bcd
fboundy <foboundy@gmail.com>
2024-04-26 20:54:29 +0100

    Add HA API

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PriceForecastAPIView, PriceForecastRegionAPIView
+from .views import PriceForecastAPIView, PriceForecastRegionAPIView, HA_PriceForecastRegionAPIView
 
 urlpatterns = [
     path("", PriceForecastAPIView.as_view()),
     path("<str:region>/", PriceForecastRegionAPIView.as_view()),
+    path("ha/<str:region>/", HA_PriceForecastRegionAPIView.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 from rest_framework import generics
 from prices.models import Forecasts
-from .serializers import PriceForecastSerializer, PriceForecastRegionSerializer
+from .serializers import PriceForecastSerializer, PriceForecastRegionSerializer, HA_PriceForecastRegionSerializer
 
 
 # class PriceHistoryAPIView(generics.ListAPIView):
@@ -33,7 +33,29 @@ class PriceForecastRegionAPIView(generics.ListAPIView):
         the user as determined by the username portion of the URL.
         """
         region = self.kwargs["region"]
-        print(region)
+
+        ids = [f.id for f in Forecasts.objects.all().order_by("-created_at")[:3]]
+
+        queryset = Forecasts.objects.filter(id__in=ids)
+        return queryset
+
+
+class HA_PriceForecastRegionAPIView(generics.ListAPIView):
+    serializer_class = HA_PriceForecastRegionSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context.update({"request": self.request})
+        context.update({"region": self.kwargs["region"]})
+        return context
+
+    def get_queryset(self):
+        """
+        This view should return a list of all the purchases for
+        the user as determined by the username portion of the URL.
+        """
+        region = self.kwargs["region"]
+
         ids = [f.id for f in Forecasts.objects.all().order_by("-created_at")[:3]]
 
         queryset = Forecasts.objects.filter(id__in=ids)
